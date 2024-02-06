c35ddc14af677e670c1858eff0c2a9f236f82dae
GadeaP <148185671+GadeaP@users.noreply.github.com>
2024-02-06 14:45:11 +0000

    terminado

--- src/app.py
+++ src/app.py
@@ -1,15 +1,70 @@
-import os
-from sqlalchemy import create_engine
+import requests
+import time
+
+url = " https://www.macrotrends.net/stocks/charts/TSLA/tesla/revenue"
+html_data = requests.get(url, time.sleep(10)).text
+
+if "403 Forbidden" in html_data:
+    headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"}
+    request = requests.get(url, headers = headers)
+    time.sleep(10)
+    html_data = request.text
+
+html_data
+
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(html_data,"html.parser")
+soup
+
+tables = soup.find_all("table")
+tables
+
 import pandas as pd
-from dotenv import load_dotenv
 
-# load the .env file variables
-load_dotenv()
+for index, table in enumerate(tables):
+    if ("Tesla Quarterly Revenue" in str(table)):
+        table_index = index
+        break
 
-# 1) Connect to the database here using the SQLAlchemy's create_engine function
+tesla_revenue = pd.DataFrame(columns = ["Date", "Revenue"])
+for row in tables[table_index].tbody.find_all("tr"):
+    col = row.find_all("td")
+    if (col != []):
+        Date = col[0].text
+        Revenue = col[1].text.replace("$", "").replace(",", "")
+        tesla_revenue = pd.concat([tesla_revenue, pd.DataFrame({
+            "Date": Date,
+            "Revenue": Revenue
+        }, index = [0])], ignore_index = True)
 
-# 2) Execute the SQL sentences to create your tables using the SQLAlchemy's execute function
+tesla_revenue.head()
+tesla_revenue = tesla_revenue[tesla_revenue["Revenue"] != ""]
+tesla_revenue.head()
 
-# 3) Execute the SQL sentences to insert your data using the SQLAlchemy's execute function
+import sqlite3
 
-# 4) Use pandas to print one of the tables as dataframes using read_sql function
+connection = sqlite3.connect("Tesla.db")
+connection
+
+cursor = connection.cursor()
+cursor.execute("""CREATE TABLE revenue (Date, Revenue)""")
+tesla_tuples = list(tesla_revenue.to_records(index = False))
+tesla_tuples[:5]
+cursor.executemany("INSERT INTO revenue VALUES (?,?)", tesla_tuples)
+connection.commit()
+
+for row in cursor.execute("SELECT * FROM revenue"):
+    print(row)
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+fig, axis = plt.subplots(figsize = (10, 5))
+
+tesla_revenue["Date"] = pd.to_datetime(tesla_revenue["Date"])
+tesla_revenue["Revenue"] = tesla_revenue["Revenue"].astype('int')
+sns.lineplot(data = tesla_revenue, x = "Date", y = "Revenue")
+
+plt.tight_layout()
+
+plt.show()
